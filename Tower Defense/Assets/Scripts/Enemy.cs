c204a8875862ae265c927c14558621cc87b671cd
Sean Crowley <crowley.p.sean@gmail.com>
2018-07-17 21:42:37 -0500

    [Ep 1] Creating Project

---
+++ Tower Defense/Assets/Scripts/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    #region Public Variables
+    public float WalkSpeed = 1.5f;
+    public float Damage = 0.01f;
+    #endregion
+
+    #region Unity Methods
+    void Update ()
+    {
+        transform.Translate(Vector3.right * Time.deltaTime * WalkSpeed, Space.World);
+    }
+    #endregion
+}
---
+++ Tower Defense/Assets/Scripts/Wall.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Wall : MonoBehaviour
+{
+    #region Game Objects
+    public Text HealthText;
+    #endregion
+
+    #region Public Variables
+    public float Health = 100f;
+    #endregion
+
+    #region Unity Methods
+    void Start ()
+    {
+        HealthText.text = string.Format("Health: {0}", Health);
+    }
+
+    void OnCollisionStay(Collision collisionInfo)
+    {
+        foreach (ContactPoint contact in collisionInfo.contacts)
+        {
+            Health -= collisionInfo.gameObject.GetComponent<Enemy>().Damage;
+
+            HealthText.text = string.Format("Health: {0}", Health);
+        }
+    }
+    #endregion
+}
