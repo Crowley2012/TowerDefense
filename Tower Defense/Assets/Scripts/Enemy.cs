56a1641eeab34c1a94bd3e89bb3c1759b3fb6222
Sean Crowley <crowley.p.sean@gmail.com>
2018-08-17 22:56:44 -0500

    Added SimpleFX Package. Added blood/smoke effects. Casing ejection.

--- Tower Defense/Assets/Scripts/Enemy.cs
+++ Tower Defense/Assets/Scripts/Enemy.cs
@@ -4,42 +4,90 @@ public class Enemy : MonoBehaviour
 {
     #region Public Fields
 
+    public GameObject BloodSplatter;
     public float Damage = 0.01f;
     public GameObject HealthText;
-    public float WalkSpeed = 1.5f;
+    public float MaxSpeed = 1.5f;
+    public float RecoverySpeed = 1f;
 
     #endregion Public Fields
 
     #region Private Fields
 
-    private float Health = 100;
+    private float _currentSpeed;
+    private float _health;
 
     #endregion Private Fields
 
     #region Unity Methods
 
-    private void OnCollisionEnter(Collision col)
+    private void OnCollisionEnter(Collision collision)
     {
-        if (!col.gameObject.name.Contains("Bullet"))
-            return;
+        if (collision.gameObject.name.Contains("Bullet"))
+        {
+            //Decrement health
+            _health -= 20;
 
-        Health -= 20;
-        HealthText.GetComponent<TextMesh>().text = Health.ToString();
+            //Show blood splatter
+            var blood = Instantiate(BloodSplatter, collision.contacts[0].point, collision.transform.rotation);
+            Destroy(blood, 2.0f);
+
+            //Decrease speed of unit when hit
+            _currentSpeed -= 0.5f;
+
+            //Decrease max speed
+            MaxSpeed -= 0.1f;
+
+            //Start movement speed recovery
+            if (!IsInvoking("Recover"))
+                InvokeRepeating("Recover", 0, RecoverySpeed);
+        }
     }
 
     private void Start()
     {
-        Health = Random.Range(100, 150);
-        HealthText.GetComponent<TextMesh>().text = Health.ToString();
+        //Set current speed
+        _currentSpeed = MaxSpeed;
+
+        //Choose a health level
+        _health = Random.Range(100, 150);
     }
 
     private void Update()
     {
-        transform.Translate(Vector3.back * Time.deltaTime * WalkSpeed, Space.World);
+        //Move unit forward
+        transform.Translate(Vector3.back * Time.deltaTime * _currentSpeed, Space.World);
 
-        if (Health <= 0)
+        //Update text
+        HealthText.GetComponent<TextMesh>().text = _currentSpeed.ToString();
+
+        //Destroy unit when killed
+        if (_health <= 0)
             Destroy(gameObject);
+
+        //Prevent backwards movement
+        if (_currentSpeed < 0f)
+            _currentSpeed = 0.25f;
     }
 
     #endregion Unity Methods
+
+    #region Private Methods
+
+    private void Recover()
+    {
+        if (_currentSpeed < MaxSpeed)
+        {
+            //Increase speed exponentially
+            _currentSpeed *= 2f;
+        }
+        else
+        {
+            //Prevent movement faster than max speed and cancel recovery
+            _currentSpeed = MaxSpeed;
+            CancelInvoke("Recover");
+        }
+    }
+
+    #endregion Private Methods
 }
--- Tower Defense/Assets/Scripts/Gun.cs
+++ Tower Defense/Assets/Scripts/Gun.cs
@@ -5,8 +5,11 @@ public class Gun : MonoBehaviour
     #region Public Fields
 
     public GameObject Bullet;
+    public GameObject Smoke;
+    public GameObject Casing;
     public Transform BulletSpawn;
-    public float test = 10000;
+    public Transform CasingSpawn;
+    public float BulletSpeed = 500;
 
     #endregion Public Fields
 
@@ -15,22 +18,34 @@ public class Gun : MonoBehaviour
     private void Fire()
     {
         // Create the Bullet from the Bullet Prefab
-        var bullet = Instantiate(Bullet, BulletSpawn.position, BulletSpawn.rotation);
+        var bullet = Instantiate(Bullet, BulletSpawn.position, Bullet.transform.rotation);
+        var smoke = Instantiate(Smoke, BulletSpawn.position, Smoke.transform.rotation);
+        var casing = Instantiate(Casing, CasingSpawn.position, Casing.transform.rotation);
 
         // Get mouse position
         RaycastHit hit;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out hit, 1000))
-            bullet.GetComponent<Rigidbody>().AddForce((hit.point - BulletSpawn.position).normalized * test);
+            bullet.GetComponent<Rigidbody>().AddForce((hit.point - BulletSpawn.position).normalized * BulletSpeed);
 
-        // Destroy the bullet after 2 seconds
+        //Add force to casing
+        casing.GetComponent<Rigidbody>().AddForce(-0.5f, 3f, 0, ForceMode.Impulse);
+
+        // Destroy objects
         Destroy(bullet, 2.0f);
+        Destroy(smoke, 3.0f);
+        Destroy(casing, 3.0f);
     }
 
     private void Update()
     {
         if (Input.GetMouseButtonUp(0))
+        {
+            //TEENER
             Fire();
+            Invoke("Fire", 0.05f);
+            Invoke("Fire", 0.1f);
+        }
     }
 
     #endregion Private Methods
