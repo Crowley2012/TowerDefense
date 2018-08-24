5b0c9ad3df0399e1893c2dbe47183a08f096e469
Sean Crowley <crowley.p.sean@gmail.com>
2018-08-24 13:31:47 -0500

    Updated Unity to 2018.2.5f1, added FPS counter

---
+++ Tower Defense/Assets/Scripts/FPS.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FPS : MonoBehaviour
+{
+    #region Public Fields
+
+    public float UpdateInterval = 0.5F;
+
+    #endregion Public Fields
+
+    #region Private Fields
+
+    private float _accumulatedTime = 0;
+    private int _frame;
+    private float _timeLeft;
+
+    #endregion Private Fields
+
+    #region Private Methods
+
+    private void Start()
+    {
+        _timeLeft = UpdateInterval;
+    }
+
+    private void Update()
+    {
+        _timeLeft -= Time.deltaTime;
+        _accumulatedTime += Time.timeScale / Time.deltaTime;
+        ++_frame;
+
+        if (_timeLeft <= 0.0)
+        {
+            float fps = _accumulatedTime / _frame;
+            gameObject.GetComponent<Text>().text = System.String.Format("{0:F0} FPS", fps);
+
+            if (fps < 30)
+                gameObject.GetComponent<Text>().material.color = Color.yellow;
+            else
+                if (fps < 10)
+                gameObject.GetComponent<Text>().material.color = Color.red;
+            else
+                gameObject.GetComponent<Text>().material.color = Color.green;
+
+            _timeLeft = UpdateInterval;
+            _accumulatedTime = 0.0F;
+            _frame = 0;
+        }
+    }
+
+    #endregion Private Methods
+}
