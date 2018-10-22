9a394164effdb15068d139294ea95f042712574b
Sean Crowley <crowley.p.sean@gmail.com>
2018-10-21 21:58:45 -0500

    Created a basic weather system

---
+++ Tower Defense/Assets/Scripts/Weather.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Weather : MonoBehaviour
+{
+    #region Public Fields
+
+    public GameObject CloudGroup1;
+    public GameObject CloudGroup2;
+    public Transform EndPosition;
+    public float Speed = 1.5f;
+
+    #endregion Public Fields
+
+    #region Private Fields
+
+    private Vector3 _startPosition;
+
+    #endregion Private Fields
+
+    #region Unity Methods
+
+    private void Start()
+    {
+        _startPosition = CloudGroup1.transform.position;
+    }
+
+    private void Update()
+    {
+        CloudGroup1.transform.Translate(Vector3.back * Time.deltaTime * Speed, Space.World);
+        CloudGroup2.transform.Translate(Vector3.back * Time.deltaTime * Speed, Space.World);
+
+        if (CloudGroup1.transform.position.z <= EndPosition.position.z)
+            CloudGroup1.transform.position = _startPosition;
+
+        if (CloudGroup2.transform.position.z <= EndPosition.position.z)
+            CloudGroup2.transform.position = _startPosition;
+    }
+
+    #endregion Unity Methods
+}
